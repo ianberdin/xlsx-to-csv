4d1b5d6c181118afd7213ee65dff54c6d8f499f4
Fawzi Abdulfattah <iifawzie@gmail.com>
2023-06-04 21:38:45 +0300

    fix: fixing the platform for aarch64

--- src/npm/get-binary.ts
+++ src/npm/get-binary.ts
@@ -14,7 +14,7 @@ function getPlatform() {
   if ( type === 'Windows_NT' ) return 'win32'
   if ( type === 'Linux' && arch === 'x64' ) return 'linux'
   if ( type === 'Darwin' && arch === 'x64' ) return 'macos-x86_64'
-  if ( type === 'Darwin' && arch === 'arm64' ) return 'aarch64'
+  if ( type === 'Darwin' && arch === 'arm64' ) return 'macos-aarch64'
 
   throw new Error(`Unsupported platform: ${ type } ${ arch }`)
 }
